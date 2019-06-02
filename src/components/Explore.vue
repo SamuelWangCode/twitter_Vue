cf2d6e8c9654ec217b258c38a17a7ff332283912
Torcx <1627301643@qq.com>
2019-06-02 17:49:32 +0800

    explore

---
+++ src/components/Explore.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="explore">
+        <div id="hottopic">
+            <p>热门话题</p>
+            <hottopics></hottopics>
+        </div>
+        <div id="tweetitems">
+            <tweets></tweets>
+        </div>
+        <div id="recommendusers">
+            <p>推荐关注</p>
+            <recommendusers></recommendusers>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import HotTopics from "./Subs/HotTopics"
+import RecommendUsers from "./Subs/RecommendUsers"
+import Tweets from "./Subs/Tweets"
+
+export default {
+    name:'explore',
+    props:{
+        
+    },
+    data(){
+        return {
+        }
+    },
+    methods:{
+    },
+    created(){
+    },
+    beforeMount() {
+    },
+    components:{
+        "tweets":Tweets,
+        "recommendusers":RecommendUsers,
+        "hottopics":HotTopics
+    }
+}
+</script>
+
+
+<style scope>
+#explore{
+    margin-top: 80px;
+}
+#hottopic{
+    width:22%;
+    float: left;
+    margin:1% 1% 1% 3%;
+    height:500px;
+    background-color: rgb(158, 158, 158);
+    font-size: 25px;
+}
+#tweetitems{
+    width:44%;
+    float: left;
+     margin:1% 2% 1% 2%;
+     height:500px;
+    background-color: rgb(175, 175, 175);
+}
+#recommendusers{
+    width:22%;
+    float: left;
+     margin:1% 3% 1% 1%;
+     height:500px;
+    background-color: #9c9c9c;
+    font-size: 25px;
+}
+</style>
+
