515c1b5c6558810a2e5c11af4e3d679a22ece083
CodeMonkeyqaq <1242532947@qq.com>
2019-07-10 10:14:47 +0800

    加入组件backTop

---
+++ src/components/Subs/BackToTop.vue
@@ -0,0 +1,13 @@
+<template>
+    <BackTop class="backTop"></BackTop>
+</template>
+<script>
+    export default {
+        name:"backToTop"
+    }
+</script>
+<style>
+.backTop{
+    z-index: 20;
+}
+</style>
